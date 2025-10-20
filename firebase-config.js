e7dd58f12c1c7fda7644676b0b8d602c4003e670
bazs.112 <62480771+ItsBaZs112@users.noreply.github.com>
2025-10-20 16:29:00 -0700

    johnald

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
- apiKey: "AIzaSyBPvb94wCfAdew9cY8Sph7Dwzq8JiK5Of4",
-
-  authDomain: "daily-dose-of-positivity-d8e78.firebaseapp.com",
-
-  projectId: "daily-dose-of-positivity-d8e78",
-
-  storageBucket: "daily-dose-of-positivity-d8e78.firebasestorage.app",
-
-  messagingSenderId: "873430812377",
-
-  appId: "1:873430812377:web:177cf4b6c2e48fc17d20c8"
+export const FB_CONFIG = {
+   projectId: "daily-dose-of-positivity-d8e78",
+   apiKey: "AIzaSyBPvb94wCfAdew9cY8Sph7Dwzq8JiK5Of4",
+   authDomain: "daily-dose-of-positivity-d8e78.firebaseapp.com",
+   databaseURL: "https://daily-dose-of-positivity-d8e78.firebaseio.com",
+   storageBucket: "daily-dose-of-positivity-d8e78.firebasestorage.app",
+   messagingSenderId: "873430812377",
+   appId: "1:873430812377:web:177cf4b6c2e48fc17d20c8"
+};
